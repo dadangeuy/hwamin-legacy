9ef554db9459727e198ea98caeebb7d7fb493823
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2018-02-18 14:24:14 +0700

    Initial Project

---
+++ src/main/java/com/rizaldi/hwamin/HwaminApplication.java
@@ -0,0 +1,12 @@
+package com.rizaldi.hwamin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwaminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwaminApplication.class, args);
+	}
+}
